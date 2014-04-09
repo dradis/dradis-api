808be7d44554582bdad2864f07902883d322a12d
Daniel Martin <etd-github@nomejortu.com>
2014-04-09 12:39:21 +0200

    Scope controllers under the Dradis:: namespace

--- app/controllers/dradis/api/api_controller.rb
+++ app/controllers/dradis/api/api_controller.rb
@@ -1,47 +1,49 @@
-module Api
-  class APIController < AuthenticatedController
-    after_filter :skip_set_cookies_header
+module Dradis
+  module API
+    class APIController < AuthenticatedController
+      after_filter :skip_set_cookies_header
 
-    protected
+      protected
 
-    # See:
-    #   http://railscasts.com/episodes/352-securing-an-api
-    def login_from_token
-      authenticate_with_http_token do |token, options|
-        User.find_by_api_token(token)
-      end
-    end
-
-    # First try to load using the API token, then use @super@ for HTTP Basic
-    # fallback
-    def current_user
-      # super
-      # @current_user ||= login_from_token || login_from_basic_auth
-      @current_user ||= login_from_token || super
-    end
-
-
-    # ---------------------------------------------------------- Authentication
-    # In API controllers, render 401 and a JSON body instead of the default
-    # defined in ApplicationController
-
-    def access_denied
-      respond_to do |format|
-        format.json do
-          render json: {message: 'Requires authentication'}, status: :unauthorized
+      # See:
+      #   http://railscasts.com/episodes/352-securing-an-api
+      def login_from_token
+        authenticate_with_http_token do |token, options|
+          User.find_by_api_token(token)
         end
       end
-    end
-    def login_required
-      !!current_user || access_denied
-    end
+
+      # First try to load using the API token, then use @super@ for HTTP Basic
+      # fallback
+      def current_user
+        # super
+        # @current_user ||= login_from_token || login_from_basic_auth
+        @current_user ||= login_from_token || super
+      end
 
 
-    # ----------------------------------------------------------- Avoid cookies
-    # In API controllers, we don't need to set any cookies (i.e. remove
-    # Set-Cookie headers).
-    def skip_set_cookies_header
-      request.session_options[:skip] = true
+      # ---------------------------------------------------------- Authentication
+      # In API controllers, render 401 and a JSON body instead of the default
+      # defined in ApplicationController
+
+      def access_denied
+        respond_to do |format|
+          format.json do
+            render json: {message: 'Requires authentication'}, status: :unauthorized
+          end
+        end
+      end
+      def login_required
+        !!current_user || access_denied
+      end
+
+
+      # ----------------------------------------------------------- Avoid cookies
+      # In API controllers, we don't need to set any cookies (i.e. remove
+      # Set-Cookie headers).
+      def skip_set_cookies_header
+        request.session_options[:skip] = true
+      end
     end
   end
 end
--- app/controllers/dradis/api/v1/nodes_controller.rb
+++ app/controllers/dradis/api/v1/nodes_controller.rb
@@ -1,39 +1,41 @@
-module Api
-  module V1
-    class NodesController < APIController
-      class Node < ::Node
-        def as_json(options={})
-          super({:only => [:id, :label, :type_id, :parent_id]}.reverse_merge(options))
+module Dradis
+  module API
+    module V1
+      class NodesController < APIController
+        class Node < ::Node
+          def as_json(options={})
+            super({:only => [:id, :label, :type_id, :parent_id]}.reverse_merge(options))
+          end
         end
-      end
 
-      respond_to :json
+        respond_to :json
 
-      def index
-        respond_with Node.order('created_at desc')
-      end
-
-      def show
-        respond_with Node.find(params[:id])
-      end
-
-      def create
-        # See:
-        #   http://blog.plataformatec.com.br/tag/respond_with/
-        node = Node.create(params[:node])
-        if node.valid?
-          respond_with(node, location: api_node_path(node))
-        else
-          respond_with(node)
+        def index
+          respond_with Node.order('created_at desc')
         end
-      end
 
-      def update
-        respond_with Node.update(params[:id], params[:node])
-      end
+        def show
+          respond_with Node.find(params[:id])
+        end
 
-      def delete
-        respond_with Node.destroy(params[:id])
+        def create
+          # See:
+          #   http://blog.plataformatec.com.br/tag/respond_with/
+          node = Node.create(params[:node])
+          if node.valid?
+            respond_with(node, location: api_node_path(node))
+          else
+            respond_with(node)
+          end
+        end
+
+        def update
+          respond_with Node.update(params[:id], params[:node])
+        end
+
+        def delete
+          respond_with Node.destroy(params[:id])
+        end
       end
     end
   end
