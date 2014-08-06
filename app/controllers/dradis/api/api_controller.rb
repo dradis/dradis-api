77dee24d2a90d05ed3f4a721f4f1b38c3858dad9
Daniel Martin <etd-github@nomejortu.com>
2014-08-06 06:37:28 +0200

    Migrate authentication to Warden)

--- app/controllers/dradis/api/api_controller.rb
+++ app/controllers/dradis/api/api_controller.rb
@@ -1,31 +1,14 @@
 module Dradis
   module API
-    class APIController < AuthenticatedController
+    class APIController < Dradis::Frontend::AuthenticatedController
       after_filter :skip_set_cookies_header
 
       protected
-
-      # See:
-      #   http://railscasts.com/episodes/352-securing-an-api
-      def login_from_token
-        authenticate_with_http_token do |token, options|
-          # We don't have the concept of users in Dradis!
-          # User.find_by_api_token(token)
-        end
-      end
-
-      # First try to load using the API token, then use @super@ for HTTP Basic
-      # fallback
-      def current_user
-        # super
-        # @current_user ||= login_from_token || login_from_basic_auth
-        @current_user ||= login_from_token || super
-      end
-
-
       # ---------------------------------------------------------- Authentication
       # In API controllers, render 401 and a JSON body instead of the default
-      # defined in ApplicationController
+      # defined in ApplicationController.
+      #
+      # This action is used as Warden's :failure_app (see engine.rb)
 
       def access_denied
         respond_to do |format|
@@ -34,10 +17,10 @@ module Dradis
           end
         end
       end
-      def login_required
-        !!current_user || access_denied
-      end
 
+      def login_required
+        warden.authenticate!(:api_auth)
+      end
 
       # ----------------------------------------------------------- Avoid cookies
       # In API controllers, we don't need to set any cookies (i.e. remove
