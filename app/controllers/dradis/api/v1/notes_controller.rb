b003e42e6850cf7d070fa3a499974675ff5cf732
Daniel Martin <etd-github@nomejortu.com>
2014-08-06 14:24:37 +0200

    Note API - adapt to Dradis::Core

--- app/controllers/dradis/api/v1/notes_controller.rb
+++ app/controllers/dradis/api/v1/notes_controller.rb
@@ -2,13 +2,13 @@ module Dradis
   module API
     module V1
       class NotesController < APIController
-        class Note < ::Note
+        class Note < ::Dradis::Core::Note
           def as_json(options={})
             super({:only => [:id, :category_id, :text]}.reverse_merge(options))
           end
         end
 
-        before_filter :find_or_initialize_node
+        before_filter :find_node
         respond_to :json
 
         def index
@@ -16,14 +16,15 @@ module Dradis
         end
 
         def show
-          respond_with @node.notes.find(params[:id])
+          respond_with @node.notes.find(params[:id].to_i)
         end
 
         def create
           # See:
           #   http://blog.plataformatec.com.br/tag/respond_with/
-          note = @node.notes.new(params[:note])
+          note = @node.notes.new(note_params)
           note.author = current_user
+          note.category = Dradis::Core::Category.default unless note.category
           note.updated_by = current_user
           note.save
           if note.valid?
@@ -34,19 +35,19 @@ module Dradis
         end
 
         def update
-          respond_with Note.update(params[:id], params[:note])
+          respond_with @node.notes.update(params[:id], note_params)
         end
 
         def destroy
-          respond_with Note.destroy(params[:id])
+          respond_with Dradis::Core::Note.destroy(params[:id].to_i)
         end
 
         private
         # For all of the operations of this controller we need to identify the Node
         # we are working with. This filter sets the @node instance variable if the
         # give :node_id is valid.
-        def find_or_initialize_node
-          @node = Node.where(id: params[:node_id].to_i).first
+        def find_node
+          @node = Dradis::Core::Node.where(id: params[:node_id].to_i).first
           unless @node
             respond_to do |format|
               format.json do
@@ -54,6 +55,10 @@ module Dradis
               end
             end
           end
+        end
+
+        def note_params
+          params.require(:note).permit(:category_id, :text)
         end
       end
     end
